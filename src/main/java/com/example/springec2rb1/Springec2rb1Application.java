08d0c410619d7bcb519f0863711f255b95b233b4
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-04-23 00:15:21 +0600

    Init java project: src/main/java/com/example/springec2rb1/Springec2rb1Application.java

---
+++ src/main/java/com/example/springec2rb1/Springec2rb1Application.java
@@ -0,0 +1,13 @@
+package com.example.springec2rb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springec2rb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springec2rb1Application.class, args);
+	}
+
+}
