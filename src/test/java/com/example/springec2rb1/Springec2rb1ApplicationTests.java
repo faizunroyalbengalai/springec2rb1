8b17eb744e419c718e4dfc9b5932cea996586b81
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-04-23 00:15:28 +0600

    Init java project: src/test/java/com/example/springec2rb1/Springec2rb1ApplicationTests.java

---
+++ src/test/java/com/example/springec2rb1/Springec2rb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springec2rb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springec2rb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
